791318cd28afa05d6a302ef8e89bf3b97d6556a2
Vanessa Konynenbelt <konyn005@umn.edu>
2022-03-14 15:19:09 -0500

    style lab

--- src/Bath.jsx
+++ src/Bath.jsx
@@ -1,6 +1,6 @@
 function Bath(props) {
   return (
-    <div className = "bath">
+    <div className = "bath" id={`${props.size}-bath`}>
       <h2>{props.size} Bath</h2>
     </div>
   );
--- src/Bedroom.jsx
+++ src/Bedroom.jsx
@@ -1,6 +1,6 @@
 function Bedroom(props) {
   return (
-    <div className='bedroom'>
+    <div className='bedroom' id={`bed-${props.bedNum}`}>
       <h2>Bedroom {props.bedNum}</h2>
     </div>
   );
